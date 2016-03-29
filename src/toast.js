a8ce4297e62a37f2c3a2d1530a64004c1f86ed64
Derek Shi <dereks@pointinside.com>
2016-03-29 10:57:41 -0700

    upgrade to beta.12

--- src/toast.js
+++ src/toast.js
@@ -1,10 +1,20 @@
-var Toast = (function () {
-    function Toast(type, message, title) {
-        this.type = type;
-        this.message = message;
-        this.title = title;
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Toast;
+    return {
+        setters:[],
+        execute: function() {
+            Toast = (function () {
+                function Toast(type, message, title) {
+                    this.type = type;
+                    this.message = message;
+                    this.title = title;
+                }
+                return Toast;
+            }());
+            exports_1("Toast", Toast);
+        }
     }
-    return Toast;
-})();
-exports.Toast = Toast;
+});
 //# sourceMappingURL=toast.js.map
