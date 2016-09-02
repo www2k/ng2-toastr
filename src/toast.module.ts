ffdfcaabfe0f6ac180a0028c79301406eeb6ebcc
Derek <dereks@pointinside.com>
2016-09-02 10:55:42 -0700

    added toastr module

---
+++ src/toast.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ToastContainer} from './toast-container.component';
+import {ToastOptions} from './toast-options';
+import {ToastsManager} from './toast-manager';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ToastContainer],
+  exports: [ToastContainer],
+  providers: [ToastOptions, ToastsManager]
+})
+export class ToastModule {
+
+}
