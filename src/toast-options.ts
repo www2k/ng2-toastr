8765deb231439bdbb16c8b98dbde5eb0082ea31b
Steven Liekens <steven.liekens@gmail.com>
2017-01-09 22:31:32 +0100

    Fix ToastOptions declaration (#116)
    * Update dev depenencies
    
    * Convert ToastOptions properties to optional
    
    Resolves  #115

--- src/toast-options.ts
+++ src/toast-options.ts
@@ -2,18 +2,18 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ToastOptions {
-  positionClass: string;
-  maxShown: number;
-  newestOnTop: boolean = false;
-  animate: string = 'fade';
+  positionClass?: string;
+  maxShown?: number;
+  newestOnTop?: boolean = false;
+  animate?: string = 'fade';
 
   // override-able properties
-  toastLife: number;
-  enableHTML: boolean = false;
-  dismiss: 'auto' | 'click' | 'controlled';
-  messageClass: string;
-  titleClass: string;
-  showCloseButton: boolean = false;
+  toastLife?: number;
+  enableHTML?: boolean = false;
+  dismiss?: 'auto' | 'click' | 'controlled';
+  messageClass?: string;
+  titleClass?: string;
+  showCloseButton?: boolean = false;
 
   constructor(options: Object) {
     Object.assign(this, options);
